4eb9f6c78eda6a58474a174e0afaab9adbb95939
rahat alom <rahatalom123@hotmail.co.uk>
2021-02-11 11:28:36 +0000

    Updates

---
+++ precache-manifest.652d9ddf73451fb524be50c23a176421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d9cc5f1136aaeb6931e622ca71a4b4",
+    "url": "/gif/index.html"
+  },
+  {
+    "revision": "47b65db4dd1b50e2d022",
+    "url": "/gif/static/js/2.1877d9b4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gif/static/js/2.1877d9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6edf6f9284f5031afc",
+    "url": "/gif/static/js/main.3999d7b4.chunk.js"
+  },
+  {
+    "revision": "beaa259049fdb49fbc2b",
+    "url": "/gif/static/js/runtime-main.6c95b6f8.js"
+  }
+]);
